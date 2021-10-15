cd71bbaeaf434165e7562b881492445858da79fe
Serenely <sgilovboris12@gmail.com>
2021-10-15 21:35:06 +0300

    for/case

---
+++ script.js
@@ -0,0 +1,16 @@
+const num = 42;
+
+switch (num) {
+    case 41:
+        console.log('Неверно');
+        break
+    case 100:
+        console.log('Неверно');
+        break
+    case 42:
+        console.log('В точку');
+        break
+    default:
+        console.log('Не в этот раз');
+        break
+}
